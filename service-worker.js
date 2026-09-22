23a04ef730f63641573d16d8c9502ec22daa2c7b
bengopsalmist-netizen <bengopsalmist@gmail.com>
2026-09-22 16:52:33 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+hereconst CACHE_NAME = "citizen-voice-v1";
+
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
