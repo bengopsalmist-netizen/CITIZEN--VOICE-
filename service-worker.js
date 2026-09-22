e2ec08f47566cb1f4fc6b14d053797f042869f12
bengopsalmist-netizen <bengopsalmist@gmail.com>
2026-09-22 23:18:46 +0300

    Update PWA cache and icons

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,33 @@
-hereconst CACHE_NAME = "citizen-voice-v1";
+hereconst CACHE_NAME = "citizen-voice-v2";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_SHELL);
+    })
   );
 
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (event) => {
